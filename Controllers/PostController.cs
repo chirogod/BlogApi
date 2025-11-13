75a6f25bbe29cd395dabae85e3c9fa8a385b36ae
Augusto Rolandelli <rolandelli_augus@hotmail.com>
2025-11-12 22:17:19 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/PostController.cs
@@ -0,0 +1,40 @@
+﻿using BlogApi.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private static List<Post> _posts = new List<Post>();
+
+        [HttpGet]
+        public IEnumerable<Post> Get()
+        {
+            return _posts;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Post> GetPost(int id)
+        {
+            return _posts.Where(p=>p.Id ==id);
+        }
+
+        [HttpPost]
+        public void Post(Post post) {
+            Post p = new Post()
+            {
+                Id = post.Id,
+                Title = post.Title,
+                Content = post.Content,
+                Category = post.Category,
+                Tags = post.Tags,
+                CreatedAt = DateOnly.FromDateTime(DateTime.Now),
+                UpdatedAt = DateOnly.FromDateTime(DateTime.Now)
+            };
+            _posts.Add(p);
+        }
+    }
+}
---
+++ Model/Post.cs
@@ -0,0 +1,13 @@
+﻿namespace BlogApi.Model
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string ? Title { get; set; }
+        public string? Content { get; set; }
+        public string? Category { get; set; }
+        public List<string>? Tags { get; set; }
+        public DateOnly CreatedAt { get; set; }
+        public DateOnly UpdatedAt { get; set; }
+    }
+}
